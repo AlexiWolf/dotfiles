bc622db03d3e066a040519dfa3e6aebcd7cb44b5
AlexiWolf <alexi@wolvesin.space>
2024-09-17 19:13:59 -0500

    Update dmenu_shutdown script
    Add command options
    
    Add sleep option

--- scripts/dmenu_shutdown.sh
+++ scripts/dmenu_shutdown.sh
@@ -1,11 +1,14 @@
 SHUTDOWN_COMMAND="systemctl poweroff"
 RESTART_COMMAND="systemctl reboot"
 HIBERNATE_COMMAND="systemctl hibernate"
+SLEEP_COMMAND="systemctl sleep"
+LOCK_COMMAND="slock"
 
 
 function prompt_shutdown {
-    case "$(printf "cancel\nlock\nhibernate\nshutdown\nrestart" | dmenu -p "Shutdown")" in
-        [lock]*     ) slock;;
+    case "$(printf "cancel\nlock\nsleep\nhibernate\nshutdown\nrestart" | dmenu -p "Shutdown")" in
+        [lock]*     ) $LOCK_COMMAND;;
+        [sleep]*     ) $SLEEP_COMMAND;;
         [hibernate]*  ) $HIBERNATE_COMMAND;;
         [shutdown]* ) $SHUTDOWN_COMMAND;;
         [restart]*  ) $RESTART_COMMAND;;
